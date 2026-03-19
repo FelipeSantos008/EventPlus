f44c4e2eab0f3fcc6218a1b20b84426e473bf849
FELIPE SILVA DOS SANTOS <90124765858@info.local>
2026-03-19 16:41:15 -0300

    Continuação da API EventPlus

--- EventPlus.WebAPI/Controllers/ComentarioEventoController.cs
+++ EventPlus.WebAPI/Controllers/ComentarioEventoController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EventPlus.WebAPI.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EventPlus.WebAPI.Controllers
+namespace EventPlus.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ComentarioEventoController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ComentarioEventoController : ControllerBase
+    private readonly IComentarioEventoRepository _comentarioEventoRepository;
+    public ComentarioEventoController(IComentarioEventoRepository comentarioEventoRepository)
     {
+        _comentarioEventoRepository = comentarioEventoRepository;
     }
+
+
 }
